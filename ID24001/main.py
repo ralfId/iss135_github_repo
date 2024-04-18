7b205a14e70794bcc5542c60bdc9213fdf5e3eb0
Rafael-ID <49356385+ralfId@users.noreply.github.com>
2024-04-18 12:23:51 -0600

    first commit, iss135

---
+++ ID24001/main.py
@@ -0,0 +1,11 @@
+def main():
+    print("")
+    print('--------------------------')
+    print('HELLO WORLD!!!')
+    print('ISS135 | UES | 2024')
+    print('--------------------------')
+    print("")
+    
+    
+    
+main()
